e4020f2f3fa001815ba4e4b968781acf8b166394
Leo <leonardozandrade@gmail.com>
2024-09-29 23:54:44 -0300

    Generic views and more

--- django_tutorial/mysite/polls/views.py
+++ django_tutorial/mysite/polls/views.py
@@ -1,23 +1,39 @@
 from django.http import HttpResponse
 from .models import Question
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5] # o - significa que ele vai ordenar do mais novo para o mais antigo
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self): # get_queryset é um método que retorna um queryset de objetos Question
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # HttpResponseRedirect é usado para redirecionar o usuário para uma página específica após uma requisição POST
+    # reverse é usado para criar uma URL para a página de resultados, ou seja, a URL que o usuário será redirecionado após votar

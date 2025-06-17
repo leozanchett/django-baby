874ae513208b398f4676181a3aa3a175e5c41c72
Leo <leonardozandrade@gmail.com>
2025-06-17 16:35:20 -0300

    Add poll templates and class-based views
    Introduced detail, index, and results HTML templates for the polls app. Refactored views to use Django's generic class-based views for listing, detail, and results pages. Updated URL patterns to use new views and added a vote handling function. This improves maintainability and aligns with Django best practices.

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"  # Define o namespace para as URLs do aplicativo 'polls'. Isso é útil para evitar conflitos de nomes entre diferentes aplicativos.
+# O 'app_name' é usado para referenciar as URLs em templates ou código Python,
+
 urlpatterns = [
-    path("", views.index, name="indess"), # O 'name' é usado para referenciar essa URL em templates ou código Python.
+    path("", views.IndexView.as_view(), name="indess"), # O 'name' é usado para referenciar essa URL em templates ou código Python.
+    # ex: /polls/5/
+    path("<int:pk>/", views.DetailView.as_view(), name="detalhes"),
+    # ex: /polls/5/results/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    # ex: /polls/5/vote/
+    path("<int:pk>/vote/", views.vote, name="vote"),
 ]
--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,6 +1,59 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from polls.models import Question, Choice
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {"latest_question_list": latest_question_list}
+#     return HttpResponse(template.render(context, request))
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+# def detail(request, question_id):
+#     try:
+#         # Tenta obter a questão com o ID fornecido
+#         question = Question.objects.get(id=question_id)
+#     except Question.DoesNotExist:
+#         # Se a questão não for encontrada, retorna uma resposta 404
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"]) # O 'choice_set' é um related manager que permite acessar as escolhas associadas a uma questão.
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1 # O F() é usado para referenciar o campo 'votes' diretamente no banco de dados, evitando a necessidade de carregar o objeto Choice inteiro na memória.
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
